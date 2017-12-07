14dd5969a4497304b3d0c988bc854538af0848f6
Aditya Mhamunkar <me@adityamhamunkar.com>
2017-12-06 19:29:54 -0500

    Initial Commit from IDE

---
+++ README.md
@@ -0,0 +1,2 @@
+# catlog
+A RESTful web application using the Python framework Flask along with implementing third-party OAuth authentication.
---
+++ database_setup.py
@@ -0,0 +1,41 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+
+class Item(Base):
+    __tablename__ = 'item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+
+
+engine = create_engine('sqlite:///catlog.db')
+
+
+Base.metadata.create_all(engine)
